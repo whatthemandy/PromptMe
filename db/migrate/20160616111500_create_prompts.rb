2cc9c42bb1a1607a311b6f3388bbd90d682c4bb0
Mandy McDorman <email": null>
2016-06-17 11:37:08 -0700

    change user_id attribute to integer

--- db/migrate/20160616111500_create_prompts.rb
+++ db/migrate/20160616111500_create_prompts.rb
@@ -2,7 +2,7 @@ class CreatePrompts < ActiveRecord::Migration
   def change
     create_table :prompts do |t|
       t.string   :idea, null: false
-      t.string   :user_id
+      t.integer  :user_id
 
       t.timestamps(null: false)
     end
